51572a853c8f4d2455f9c4a64f464ce7090358b7
frynor <leolomtadze6002@gmail.com>
2025-02-05 17:58:12 +0400

    view matrix

--- src/cameraPerspective.c
+++ src/cameraPerspective.c
@@ -13,3 +13,11 @@ Camera* camera_initialize() {
 
 	return cam;
 }
+
+void camera_calculate_view_matrix(Camera *camera) {
+	matrix44_identity(&camera->viewMatrix);
+	rotate(&camera->viewMatrix, &((Vector3){1.0f, 0.0f, 0.0f}), camera->rotation.x);
+	rotate(&camera->viewMatrix, &((Vector3){0.0f, 1.0f, 0.0f}), camera->rotation.y);
+	rotate(&camera->viewMatrix, &((Vector3){.0f, 0.0f, 1.0f}), camera->rotation.z);
+	translate(&camera->viewMatrix, &camera->position);
+}
